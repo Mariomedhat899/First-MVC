a46928a30420e4db7ec9619395f327cd8fd926f6
Mariomedhat899 <mariomedhat899@gmail.com>
2025-09-26 22:27:05 +0300

    Department.BLL

---
+++ Company.G02.BLL/InterFaces/IDepartmentReposaitory.cs
@@ -0,0 +1,26 @@
+﻿using Company.G02.DAL.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Company.G02.BLL.InterFaces
+{
+    public interface IDepartmentReposaitory
+    {
+        IEnumerable<Department> GetALl();
+
+        Department? Get(int Id);
+
+        int Add(Department model);
+
+        int Update(Department model);
+
+        int Delete(Department model);
+
+
+
+    }
+}
---
+++ Company.G02.BLL/Repos/DepartmentReposaitory.cs
@@ -0,0 +1,57 @@
+﻿using Company.G02.BLL.InterFaces;
+using Company.G02.DAL.Modules;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G02.DAL.Data.Contexts;
+
+namespace Company.G02.BLL.Repos
+{
+    public class DepartmentReposaitory : IDepartmentReposaitory
+    {
+
+        private readonly CompanyDbContext _context;
+
+        public DepartmentReposaitory()
+        {
+            _context = new CompanyDbContext();
+        }
+        IEnumerable<Department> IDepartmentReposaitory.GetALl()
+        {
+            return _context.Departments.ToList();
+        }
+
+        Department? IDepartmentReposaitory.Get(int Id)
+        {
+            return _context.Departments.Find(Id);
+        }
+
+        int IDepartmentReposaitory.Add(Department model)
+        {
+
+            _context.Departments.Add(model);
+
+            return _context.SaveChanges();
+        }
+
+        int IDepartmentReposaitory.Update(Department model)
+        {
+
+            _context.Departments.Update(model);
+
+            return _context.SaveChanges();
+
+        }
+
+        int IDepartmentReposaitory.Delete(Department model)
+        {
+
+            _context.Departments.Remove(model);
+
+            return _context.SaveChanges();
+        }
+    }
+}
