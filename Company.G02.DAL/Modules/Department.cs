473677d00280e60742db634f40f3939e6934fe6c
Mariomedhat899 <mariomedhat899@gmail.com>
2025-09-26 21:56:27 +0300

    Impelementaion OF DAL

---
+++ Company.G02.DAL/Modules/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Modules
+{
+    public class Department
+    {
+        public required int Id { get; set; };
+        public  string Name { get; set; };
+        public string Code { get; set; };
+
+
+        public DateTime CreateAt { get; set; };
+    }
+}
